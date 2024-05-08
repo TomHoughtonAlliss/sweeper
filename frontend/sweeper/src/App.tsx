1a17da9bd8351c0680e40ee802cc0c541bf652cc
Tom Houghton-Alliss <tom.houghton-alliss@jamescape.net>
2024-05-08 16:56:53 +0100

    Removed some default React stuff

--- frontend/sweeper/src/App.tsx
+++ frontend/sweeper/src/App.tsx
@@ -1,27 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-        <div className="App-difficulty-parent">
+        Sweeper
+      </header>
+      <div className="App-difficulty-parent">
           <div className='App-difficulty-box'>Easy</div>
           <div className='App-difficulty-box'>Medium</div>
           <div className='App-difficulty-box'>Hard</div>
+          <div className='App-difficulty-box'>Expert</div>
         </div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
