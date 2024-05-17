4306f183d7bbf300c43dcf0ad3970237c0ec9997
Tom Houghton-Alliss <tom.houghton-alliss@jamescape.net>
2024-05-17 17:31:22 +0100

    Formatted

--- frontend/src/components/Scoreboard.tsx
+++ frontend/src/components/Scoreboard.tsx
@@ -1,45 +1,59 @@
-import { faker } from '@faker-js/faker';
+import { faker } from "@faker-js/faker";
 
 function getScore(): string[] {
-    const name: string = faker.person.firstName();
-    const time: string = String(Math.floor(Math.random() * 100));
-    const date: string = "14/02/1994";
+  const name: string = faker.person.firstName();
+  const time: string = String(Math.floor(Math.random() * 100));
+  const date: string = "14/02/1994";
 
-    const score: string[] = [name, time, date];
+  const score: string[] = [name, time, date];
 
-    return score;
-
+  return score;
 }
 
 function getScoreBoard(): string[][] {
-    const scores: string[][] = [];
+  const scores: string[][] = [];
 
-    for (let i = 0; i < 10; i++) {
-        scores.push(getScore());
-    }
+  for (let i = 0; i < 10; i++) {
+    scores.push(getScore());
+  }
 
-    return scores;
+  return scores;
 }
 
 export default function ScoreBoard() {
-    const scoreBoard: string[][] = getScoreBoard();
-    
-    return (<div style={{}}>
-        <table style={{marginLeft: "auto", marginRight: "auto", textAlign: "left", border: "1px solid gray"}}>
+  const scoreBoard: string[][] = getScoreBoard();
+
+  return (
+    <div style={{}}>
+      <table
+        style={{
+          marginLeft: "auto",
+          marginRight: "auto",
+          textAlign: "left",
+          border: "1px solid gray",
+        }}
+      >
+        <tr>
+          <th>Name</th>
+          <th>Time (seconds)</th>
+          <th>Date</th>
+        </tr>
+        {scoreBoard.map((row: string[], yIndex: number) => {
+          return (
             <tr>
-                <th>Name</th>
-                <th>Time (seconds)</th>
-                <th>Date</th>
+              <td style={{ paddingRight: "10rem", border: "1px solid gray" }}>
+                {row[0]}
+              </td>
+              <td style={{ paddingRight: "10rem", border: "1px solid gray" }}>
+                {row[1]}
+              </td>
+              <td style={{ paddingRight: "10rem", border: "1px solid gray" }}>
+                {row[2]}
+              </td>
             </tr>
-            {scoreBoard.map(
-                (row: string[], yIndex: number) => {
-                    return <tr>
-                        <td style={{paddingRight: "10rem", border: "1px solid gray"}}>{row[0]}</td>
-                        <td style={{paddingRight: "10rem", border: "1px solid gray"}}>{row[1]}</td>
-                        <td style={{paddingRight: "10rem", border: "1px solid gray"}}>{row[2]}</td>
-                    </tr>
-                }
-            )}
-        </table>
-    </div>)
+          );
+        })}
+      </table>
+    </div>
+  );
 }
