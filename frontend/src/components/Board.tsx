b0a2082a8bf468892f56570d33c11adfe3ab503c
Tom Houghton-Alliss <tom.houghton-alliss@jamescape.net>
2024-05-20 09:30:54 +0100

    Added cell object

--- frontend/src/components/Board.tsx
+++ frontend/src/components/Board.tsx
@@ -1,14 +1,27 @@
+type Cell = {
+  revealed: boolean;
+  isMine: boolean;
+  isFlagged: boolean;
+  adjacentMines: number;
+};
+
 function instantiateBoard(
   width: number,
   height: number,
   bombs: number
-): number[][] {
-  const board: number[][] = [];
+): Cell[][] {
+  const board: Cell[][] = [];
 
   for (let i = 0; i < height; i++) {
-    const row: number[] = [];
+    const row: Cell[] = [];
     for (let j = 0; j < width; j++) {
-      row.push(0);
+      let cell: Cell = {
+        revealed: false,
+        isMine: false,
+        isFlagged: false,
+        adjacentMines: 0,
+      }
+      row.push(cell);
     }
     board.push(row);
   }
@@ -20,15 +33,17 @@ export default function Board() {
   let board = instantiateBoard(10, 10, 10);
 
   return (
-    <div style={{ 
+    <div
+      style={{
         display: "inline-flex",
         margin: "2rem",
-        border: "1px solid gray"
-        }}>
-      {board.map((row: number[], yIndex: number) => {
+        border: "1px solid gray",
+      }}
+    >
+      {board.map((row: Cell[], yIndex: number) => {
         return (
           <div style={{}}>
-            {row.map((cell: number, xIndex: number) => {
+            {row.map((cell: Cell, xIndex: number) => {
               return (
                 <div
                   style={{
@@ -41,7 +56,7 @@ export default function Board() {
                     backgroundColor: "#DCDCDC",
                   }}
                 >
-                  {cell}
+                  {cell.adjacentMines}
                 </div>
               );
             })}
