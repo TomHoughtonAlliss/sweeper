a0931774db18907b788e1c420f16bb0c19223c85
Tom Houghton-Alliss <tom.houghton-alliss@jamescape.net>
2024-10-23 15:20:59 +0100

    Created a timer using a couple of different hooks

--- frontend/src/components/Timer.tsx
+++ frontend/src/components/Timer.tsx
@@ -1,54 +1,30 @@
-import { useState, useEffect, useImperativeHandle, forwardRef } from 'react';
+import { useEffect, useState } from "react";
 
-const Timer = forwardRef((props, ref) => {
-    const [isRunning, setIsRunning] = useState(false);
-    const [time, setTime] = useState(0);
-    const [intervalId, setIntervalId] = useState<NodeJS.Timeout | null>(null)
+export default function Timer({
+  startTime,
+  stopped,
+}: {
+  startTime: number;
+  stopped: boolean;
+}) {
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(0);
 
-    useEffect(() => {
+  const getTime = () => {
+    const elapsed = Date.now() - startTime;
 
-        if (isRunning) {
-            const id = setInterval(() => {
-              setTime(prevTime => prevTime + 1);
-            }, 1000);
-            setIntervalId(id);
+    setSeconds(Math.floor((elapsed / 1000) % 60));
+    setMinutes(Math.floor(elapsed / 1000 / 60));
+  };
 
-        } else if (!isRunning && intervalId) {
-            clearInterval(intervalId);
-            setIntervalId(null);
-        }
-        return () => {
-            if (intervalId) {
-                clearInterval(intervalId);
-            }
-        };
-    }, [isRunning]);
-
-    useImperativeHandle(ref, () => ({
-        start: () => setIsRunning(true),
-        stop: () => setIsRunning(false),
-        reset: () => setTime(0),
-    }))
-
-    const formatTime = (time: number) => {
-        const minutes = Math.floor(time / 60);
-        const seconds = time % 60;
-        return `${minutes}:${seconds > 10 ? seconds : `0${seconds}`}`
+  useEffect(() => {
+    if (!stopped) {
+        const interval = setInterval(() => {
+            getTime();
+        }, 1000);
+        return () => clearInterval(interval);
     }
+  })
 
-    return (
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            border: "solid 1px",
-            backgroundColor: "#CDCDCD"
-          }}
-        >
-            {formatTime(time)}
-        </div>
-    )
-})
-
-export default Timer
+  return <div>{`${minutes <= 9 ? "0" : ""}${minutes}:${seconds <= 9 ? "0" : ""}${seconds}`}</div>;
+}
