bed7d7fcbfb98b60a1fd49a6f46c2e77f824f612
Tom Houghton-Alliss <tom.houghton-alliss@jamescape.net>
2024-10-24 11:39:33 +0100

    Removed minutes, and made the timer a three-digit display

--- frontend/src/components/Timer.tsx
+++ frontend/src/components/Timer.tsx
@@ -8,19 +8,17 @@ export default function Timer({
   stopped: boolean;
 }) {
   const [seconds, setSeconds] = useState(0);
-  const [minutes, setMinutes] = useState(0);
 
   useEffect(() => {
     if (!stopped) {
         const interval = setInterval(() => {
           const elapsed = Date.now() - startTime;
 
-          setSeconds(Math.floor((elapsed / 1000) % 60));
-          setMinutes(Math.floor(elapsed / 1000 / 60));
+          setSeconds(Math.floor(elapsed / 1000));
         }, 1000);
         return () => clearInterval(interval);
     }
   }, [stopped, startTime]);
 
-  return <div>{`${minutes <= 9 ? "0" : ""}${minutes}:${seconds <= 9 ? "0" : ""}${seconds}`}</div>;
+  return <div>{`${seconds <= 99 ? "0" : ""}${seconds <= 9 ? "0" : ""}${seconds}`}</div>;
 }
