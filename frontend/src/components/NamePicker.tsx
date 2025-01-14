521a115efc99cc3d793a1c57cc67062adad22e16
TomHoughtonAlliss <tomhoughton18@gmail.com>
2025-01-14 23:31:08 +0000

    Added name useEffect and formatted

--- frontend/src/components/NamePicker.tsx
+++ frontend/src/components/NamePicker.tsx
@@ -1,18 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Picker from "./Picker";
 
 
 
-export default function NamePicker() {
-	const [a, setA] = useState(0);
-	const [b, setB] = useState(0);
-	const [c, setC] = useState(0);
+export default function NamePicker({ setter }: { setter: (name: string) => void }) {
+  const [a, setA] = useState(0);
+  const [b, setB] = useState(0);
+  const [c, setC] = useState(0);
 
-	return (
-		<div className="name-picker" style={{ display: "flex", fontFamily: "monospace" }}>
-			<Picker value={a} setter={setA}/>
-			<Picker value={b} setter={setB}/>
-			<Picker value={c} setter={setC}/>
-		</div>
-	);
+  useEffect(() => {
+    setter(`${a}${b}${c}`);
+  }, [a, b, c])
+
+  return (
+    <div className="name-picker" style={{ display: "flex", fontFamily: "monospace" }}>
+      <Picker value={a} setter={setA} />
+      <Picker value={b} setter={setB} />
+      <Picker value={c} setter={setC} />
+    </div>
+  );
 }
