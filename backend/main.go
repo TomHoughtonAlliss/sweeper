dd43dfcd4fd10ffb5ed27565782b79fe4d1b81e0
Tom <tom@harkerapp.com>
2025-01-14 15:12:26 +0000

    Working hard (hardly working)

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
-	fmt.Println("Hello! This is where the server will be.")
+	if err := godotenv.Load(".env"); err != nil && !os.IsNotExist(err) {
+		panic("failed to load .env")
+	}
+
+	e := echo.New()
+
+	e.Debug = true
+
+	address := os.Getenv("SERVER_URL")
+
+	e.Logger.Fatal(e.Start(address))
 }
