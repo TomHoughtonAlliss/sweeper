f24e1544db1a8b9186e5dd4180bbfadede0df897
TomHoughtonAlliss <tomhoughton18@gmail.com>
2025-01-15 09:45:12 +0000

    Returned the proper type from my query

--- frontend/src/requests/get_scores.ts
+++ frontend/src/requests/get_scores.ts
@@ -1,9 +1,17 @@
 import { useQuery } from 'react-query';
 import axios from 'axios';
 
+type Score = {
+	id: string;
+	name: string;
+	time: number;
+	date: string;
+};
+
 const fetchScores = async () => {
     const { data } = await axios.get('http://localhost:8000/scores');
-    return data;
+
+    return data as Score[];
 };
 
 export const useScores = () => {
